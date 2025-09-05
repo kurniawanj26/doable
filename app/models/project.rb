d4459f618a56d04f54f9fae5418726b28b64bd0a
“kurniawanj26” <jayadi.kurniawan@gmail.com>
2025-09-06 05:29:20 +0700

    one to many relationship
    - feat : add one to many relationship for project and todo
    - feat : migrate db

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,8 @@
 class Project < ApplicationRecord
+  # project can have > 1 todo
+  # dependent: :destroy = when a project is deleted, this will also delete all of its associated todos gets deleted as well
+  has_many :todos, dependent: :destroy
+
   # field name is mandatory / can't be blank
   validates :name, presence: {message: "Did you forget to add a name?"}
 end
